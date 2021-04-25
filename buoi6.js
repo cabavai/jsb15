53018039b080c19718a9b301b269466c6ee83854
cabavai <82050700+cabavai@users.noreply.github.com>
2021-04-25 20:47:31 +0700

    Add files via upload

---
+++ buoi6.js
@@ -0,0 +1,10 @@
+let classes = [{name : "dungx", age : 16} ,{name : "huy" , age : 17}, {name: "bla" , age : 111} ]
+let total = 0
+console.log(classes.length)
+for (i = 0; i < classes.length; i++) {
+  total += classes[i].age ;
+}
+
+console.log(total)
+
+
