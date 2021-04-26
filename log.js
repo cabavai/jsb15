4a007497ba00132e70d4ce1e135d91958a2e1e8b
cabavai <82050700+cabavai@users.noreply.github.com>
2021-04-26 23:12:45 +0700

    Add files via upload

---
+++ log.js
@@ -0,0 +1,16 @@
+function b() {
+    
+    var dn = document.getElementById("dn").value;
+    var mk = document.getElementById("mk").value;
+    if (dn=="Dung@gmail.com"||mk=="123") {
+        alert("Đăng nhập thành công")
+    
+    }
+      
+   
+    
+
+ else {
+     alert ("Sai thông tin")
+ }
+}
