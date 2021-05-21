88137817f16e4ec418dee8a18a38deeab246d9d4
cabavai <82050700+cabavai@users.noreply.github.com>
2021-05-21 15:33:35 +0700

    Add files via upload

---
+++ dmauu.js
@@ -0,0 +1,17 @@
+function xanh() {
+  document.getElementById("gr").value =  document.body.style.backgroundColor = 'green'
+
+  
+}
+function hong() {
+  document.getElementById("p").value =  document.body.style.backgroundColor = 'pink'
+}
+function vang() {
+  document.getElementById("p").value =  document.body.style.backgroundColor = 'yellow'
+}
+function ddo() {
+  document.getElementById("p").value =  document.body.style.backgroundColor = 'red'
+}
+function xanhx() {
+  document.getElementById("p").value =  document.body.style.backgroundColor = 'blue'
+}
